43776ec5c0f373f8c4dbe3208b58cf35f6b87877
ChristoferMendes <christoferluizdsm@gmail.com>
2023-02-22 20:46:17 -0300

    rename handlePrices() to getPrices() and refactor handleStoreLinks() to getStoreLinkXPaths()

--- src/modules/handlePrices.ts
+++ src/modules/handlePrices.ts
@@ -1,6 +1,6 @@
 import { Page } from "puppeteer";
 
-export const handlePrices = (page: Page) => {
+export const getPrices = (page: Page) => {
   const priceSelector = '.hn9kf > span'
   return page.$$eval(priceSelector, (el) => el.slice(0, 7).map(item => item.innerText))
 }
--- src/modules/handleStoreLinks.ts
+++ src/modules/handleStoreLinks.ts
@@ -1,10 +1,11 @@
-export const handleStoreLinks = () => {
-  const linksNumbers = [1, 2, 3, 4, 5, 6, 7, 8]
-  const linksXPaths = linksNumbers.map(number => {
-    const numberToString = String(number);
+export const getStoreLinkXPaths = () => {
+  const MAX_ITEMS = 7;
+  const linksXPaths = Array.from({ length: MAX_ITEMS }, (_, i) => {
+    const itemIndex = i + 1;
+    const linkXPath = `/html/body/div[7]/div/div[4]/div[3]/div/div[3]/div[1]/g-scrolling-carousel/div[1]/div/div/div[${itemIndex}]/a`;
 
-    return `/html/body/div[7]/div/div[4]/div[3]/div/div[3]/div[1]/g-scrolling-carousel/div[1]/div/div/div[${numberToString}]/a`
-  })
+    return linkXPath;
+  });
 
   return linksXPaths
 }
