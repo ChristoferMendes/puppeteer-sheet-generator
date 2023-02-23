80fd81545803098903cd3c8a56786eb30239ca8f
ChristoferMendes <christoferluizdsm@gmail.com>
2023-02-23 20:03:16 -0300

    update pcItems with new motherboard and processor

--- src/store/pcItems.ts
+++ src/store/pcItems.ts
@@ -1,7 +1,7 @@
 export const pcItems = ['Fonte Gamemax GS600, 600W, 80 Plus White, PFC Ativo, Black',
-                       'Placa de Vídeo Galax NVIDIA GeForce RTX 3060 (1-Click OC), LHR, 12GB, GDDR6, DLSS, Ray Tracing',
                        'Placa de Vídeo RTX 3060 Ti 1-Click OC Galax NVIDIA GeForce, 8GB, GDDR6, LHR, DLSS, Ray Tracing',
                        'Placa de Vídeo PowerColor Hellhound AMD Radeon RX 6650 XT, 8GB, GDDR6, FSR, Ray Tracing, AXRX 6650 XT 8GBD6-3DHL/OC',
-                       'Placa Mãe Asus Prime A520M-A, Chipset A520, AMD AM4, mATX, DDR4, 90MB14Z0-M0EAY0',
+                       'Placa Mãe Asus Prime A520M-E, Chipset A520, AMD AM4, mATX, DDR4, 90MB14Z0-M0EAY0',
                        'Processador AMD Ryzen 5 4500 3.6GHz (4.1GHz Turbo), 6-Cores 12-Threads, Cooler Wraith Stealth, AM4, 100-100000644BOX',
+                       'Processador AMD Ryzen 5 5500, 3.6GHz (4.2GHz Max Turbo), Cache 19MB, AM4, Sem Vídeo'
                     ]
