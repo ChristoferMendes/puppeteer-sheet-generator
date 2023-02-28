83418bbe3c315b25acd8c1cc87f5d10517e70936
ChristoferMendes <christoferluizdsm@gmail.com>
2023-02-27 22:24:10 -0300

    add environment variables to store spreadsheet ID and update index.ts to use it

--- index.ts
+++ index.ts
@@ -1,25 +1,3 @@
-// import * as XLSX from 'xlsx'
-// import puppeteer from 'puppeteer'
-
 import { bootstrap } from "./src";
-
-
-// (() => {
-//   let row = [
-//     ['test', 'test2',],R$ 179,99 Custava R$ 211,75.
-//     ['value', 'value2']
-//   ];
-
-
-//   const workSheet = XLSX.utils.aoa_to_sheet(row, {cellStyles: true})
-
-//   const workBook = XLSX.utils.book_new()
-
-//   XLSX.utils.book_append_sheet(workBook, workSheet, 'Sheet 1')
-
-//   XLSX.writeFile(workBook, './src/sheets/test2.xlsx')
-// })()
- 
-
 
 bootstrap()
--- src/modules/uploadToGoogleSheet/index.ts
+++ src/modules/uploadToGoogleSheet/index.ts
@@ -1,8 +1,10 @@
 import { logUpdatedSheet } from '../../logger';
 import { initializeGoogleAPI } from './initializeGoogleAPI';
+import { config } from 'dotenv'
+config()
 
 export const uploadToGoogleSheet = async (fileData: (string | number | string[])[][]) => {
-  const spreadsheetId = "1Ih4EbfV6VjW3J3ltQYdFDnE7ZyxW9K5anVIyjTqRdK4"
+  const spreadsheetId = process.env.SPREAD_SHEET_ID;
   const range = `A1:C${fileData.length}`;
 
   const { googleSheets } = await initializeGoogleAPI()
