57a6e34fc85e81bb3eb76241d3e68db06f778cf9
ChristoferMendes <christoferluizdsm@gmail.com>
2023-03-08 15:01:06 -0300

    update uploadToGoogleSheet to include total price calculation and add to spreadsheet

--- src/modules/uploadToGoogleSheet/index.ts
+++ src/modules/uploadToGoogleSheet/index.ts
@@ -1,24 +1,45 @@
 import { logUpdatedSheet } from '../../logger';
 import { initializeGoogleAPI } from './initializeGoogleAPI';
 import { config } from 'dotenv'
+import { getTotalPrices } from '../getTotalPrice';
 config()
 
-export const uploadToGoogleSheet = async (fileData: (string | number | string[])[][]) => {
+export const uploadToGoogleSheet = async (fileData: (string | number)[][]) => {
   const spreadsheetId = process.env.SPREAD_SHEET_ID;
   const range = `A1:C${fileData.length}`;
+  const valueInputOption = 'USER_ENTERED'
 
   const { googleSheets } = await initializeGoogleAPI()
+
+  await googleSheets.spreadsheets.values.clear({
+    spreadsheetId,
+    range: 'A1:ZZ'
+  })
 
   await googleSheets.spreadsheets.values.update({
     spreadsheetId,
     range,
-    valueInputOption: 'USER_ENTERED',
+    valueInputOption,
     requestBody: {
       values: fileData,
     },
   })
+
+  const totalPrice = getTotalPrices(fileData)
+  const totalRow = [['TOTAL PRICE', totalPrice]]
+  const lastRow = `A${fileData.length + 1}:B${fileData.length + 1}`
+
+  await googleSheets.spreadsheets.values.update({
+    spreadsheetId,
+    range: lastRow,
+    valueInputOption,
+    requestBody: {
+      values: totalRow
+    }
+  })
   
 
   logUpdatedSheet()
+  console.log('TOTAL:', totalPrice)
   process.exit()
 }
